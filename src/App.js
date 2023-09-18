ffddbbf7b63a1a5d2112c9d1062f9d931282ee51
Shahriyar <growbro405@gmail.com>
2023-09-18 20:40:30 +0500

    first commit.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// components
+
+import Header from './components/Header';
+import Home from './components/Homell';
+import Section from './components/Section';
+
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Section/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Styled Componets
+import styled from 'styled-components'
+
+
+function Home() {
+  return (
+    <Container>
+      Home
+    </Container>
+  )
+}
+
+export default Home
